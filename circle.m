6c93129d0642fe2981994a46ec35f197a5bf8dab
Austin Stanley <r.austin.stanley@gmail.com>
2015-11-30 21:20:45 -0600

    Initial commit

---
+++ circle.m
@@ -0,0 +1,15 @@
+function [h] = circle(r, x, y, fmt)
+%CIRCLE Plot a circle on the current axes
+%   Plot a circle with radius r
+%   centered at (x, y)
+%
+%   Returns a handle to the plot
+
+hold on
+th = 0:pi/50:2*pi;
+
+xunit = r * cos(th) + x;
+yunit = r * sin(th) + y;
+h = plot(xunit, yunit, fmt);
+end
+
