5bdee189983851cbcad1c1bbad48c31ed4a03513
Austin Stanley <r.austin.stanley@gmail.com>
2016-01-25 17:03:27 -0600

    comments

--- soundwave.m
+++ soundwave.m
@@ -6,10 +6,14 @@ function soundwave(h, k)
     mics = [150 0; 0 150; -150 0; 0 -150];
     tolerance = 5.625;
     
+    % speed of sound: 11.25 feet per centisecond
     for r = 0.1:11.25:499.96
         delete(c);
         c = circle(r, h, k, 'r');
         
+        % detect soundwave collisions with microphones
+        % using the standard equation for a circle
+        % (x-h)^2 + (y-k)^2 = r^2
         for m = 1:1:4
             lhs = (mics(m,1) - h)^2 + (mics(m,2) - k)^2;
             if lhs > (r - tolerance)^2 && lhs < (r + tolerance)^2
